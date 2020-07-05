07b32b479b185840c47194f7d316b9cc63558c47
liyuan0514 <724837404@qq.com>
2020-07-06 00:07:14 +0800

    延迟队列

---
+++ mq-rabbitmq-template-server/src/main/java/com/xiaoyuan/mq/rabbitmq/template/server/delay/DelayParamConstant.java
@@ -0,0 +1,15 @@
+package com.xiaoyuan.mq.rabbitmq.template.server.delay;
+
+/**
+ * @author liyuan
+ * @description 延时队列
+ */
+public interface DelayParamConstant {
+
+    static final String EXCHANGE_NAME = "test_exchange_delay";
+
+    static final String QUEUE_NAME_ONE = "test_queue_delay_1";
+
+    static final String QUEUE_NAME_TWO = "test_queue_delay_2";
+
+}
---
+++ mq-rabbitmq-template-server/src/main/java/com/xiaoyuan/mq/rabbitmq/template/server/delay/DelayProvider.java
@@ -0,0 +1,50 @@
+package com.xiaoyuan.mq.rabbitmq.template.server.delay;
+
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+/**
+ * @author liyuan
+ * @description 延时队列
+ */
+@RestController
+public class DelayProvider {
+
+    @Autowired
+    private RabbitTemplate rabbitTemplate;
+
+    //回调函数: confirm确认
+    final RabbitTemplate.ConfirmCallback confirmCallback = (correlationData, ack, cause) -> {
+        if (ack) {
+            //如果confirm返回成功 则进行更新
+            System.out.println("推送成功.....");
+        } else {
+            //失败则进行具体的后续操作:重试 或者补偿等手段
+            System.err.println("异常处理...");
+        }
+    };
+
+    @GetMapping("/delay")
+    public String send(int i) {
+        String date = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new Date());//24小时制
+        String message = "hello " + date;
+
+        String routingKey;
+        // 两个都可以消费
+        routingKey = "delete";
+        // 通过实现 ConfirmCallback 接口，消息发送到 Broker 后触发回调，确认消息是否到达 Broker 服务器，也就是只确认是否正确到达 Exchange 中
+        rabbitTemplate.setConfirmCallback(confirmCallback);
+        System.out.println("Sender : " + message);
+        this.rabbitTemplate.convertAndSend(DelayParamConstant.EXCHANGE_NAME, routingKey, message, message1 -> {
+            message1.getMessageProperties().setHeader("x-delay", 30000);//设置延迟时间
+            return message1;
+        });
+        return "success";
+    }
+
+}
