b06cd5307564b77b102ec2d633cd3f3f8120f889
liyuan <xiaoyuan.lee@foxmail.com>
2020-07-10 01:02:58 +0800

    netty

---
+++ netty/src/test/java/com/xiaoyuan/netty/NettyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xiaoyuan.netty;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NettyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
